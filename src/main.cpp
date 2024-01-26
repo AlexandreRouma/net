e5d74c3d31db09dc927092e149134ec6ac5804d2
AlexandreRouma <alexandre.rouma@gmail.com>
2024-01-26 17:14:30 +0100

    fix UDP broadcast and http protocol warnings

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,9 @@
 #include <thread>
 #include <string>
 #include <fstream>
-#include "proto/rigctl.h"
+#include "proto/http.h"
 
 int main() {
-    try {
-        auto client = net::rigctl::connect("172.19.13.134");
-
-        printf("%d\n", client->getAntenna());
-        int ret = client->setAntenna(1);
-        printf("ret: %d\n", ret);
-        printf("%d\n", client->getAntenna());
-
-        client->close();
-    }
-    catch (std::runtime_error& e) {
-        fprintf(stderr, "Error: %s\n", e.what());
-    }
+    
     return 0;
 }
